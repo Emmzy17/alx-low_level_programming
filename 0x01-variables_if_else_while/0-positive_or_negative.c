5bcfb1aa82276d718e69f3ac337c40dd6ffd5f24
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-14 12:45:27 -0700

    positive-negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,18 +5,14 @@
 /**
  * File_name : 0-positive_of_negative
  * Authour : Emmanuel Adoyi
- *
  */
-
-/* betty style doc for function main goes there */
+/**
+ * Main : this function checks whether a random number is positivee or negative or zero
+ * Parameters : N is a random number integer
+ * Return: 0- Success
+ */                
 int main(void)
 {
-	/**
-	 * Main : this function checks whether a random number is positive
-	 *
-	 * Prints: N is positive or negative or zero
-	 * Return 0: Success
-	 */
 	int n;
 
 	srand(time(0));
