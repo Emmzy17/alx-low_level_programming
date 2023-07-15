2d0a0ce68f672876d388464727ce4947a2be04b1
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-15 05:33:31 -0700

    print-letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - Entry point
  * Description: 'Prints Alphabetical characters'
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
-	
 	int i;
-	
-	for ( i = 0; alpha[i]== '\0'; i++)
+	for ( i = 0; alpha[i] != '\0'; i++)
 	{
 		putchar(alpha[i]);
 	}	
 	
 	putchar('\n');
 	
-	return 0;
+	return (0);
 }
