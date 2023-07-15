56c98efa07a0449ce3fa46eae8ad69c84b923f91
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-15 06:47:51 -0700

    reverse-alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: 'Prints Alphabetical characters'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[] = "zyxwvutsrqponmlkjihgedcba";
+	int i;
+
+	for (i = 0; alpha[i] != '\0'; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
