efda8976e1b82be1375aa5e026f2dbbfc1281787
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-15 05:57:24 -0700

    print-letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: 'Prints Alphabetical characters in lower and uperrcase'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+
+        for (i = 0; alpha[i] != '\0'; i++)
+        {
+                putchar(alpha[i]);
+        }
+        putchar('\n');
+        return (0);
+}
