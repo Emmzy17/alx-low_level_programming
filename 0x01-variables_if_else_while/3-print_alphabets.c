15b7a0ec92b24ed9557293ed1c0b4309571d33a7
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-15 05:59:37 -0700

    print-letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,12 @@
 int main(void)
 {
         char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int i;
-
-        for (i = 0; alpha[i] != '\0'; i++)
-        {
-                putchar(alpha[i]);
-        }
-        putchar('\n');
-        return (0);
+	int i;
+	
+	for (i = 0; alpha[i] != '\0'; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
 }
