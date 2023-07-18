194aa67f8702fb41828bebf2d38c922da7b0e21f
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-18 09:40:12 -0700

    putchar-

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,9 @@ int main(void)
 {
 	int i;
 	char putchar[] = "_putchar";
-	for(i=0; i != '\0'; i++)
+	for(i=0; putchar[i] != '\0'; i++)
 	{
-		_putchar(i);
+		_putchar(putchar[i]);
 	}
 	_putchar('\n');
 	return 0;
