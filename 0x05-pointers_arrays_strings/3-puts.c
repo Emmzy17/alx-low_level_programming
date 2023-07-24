5c54c3172402383694f6d9499c23fdbf8fdd4f74
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-24 06:33:26 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts(char *str) - Prints a string
+ * @str : Address of the string
+ * Return: 0 is always sucess
+ */
+
+void _puts(char *str)
+{
+	while(*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
