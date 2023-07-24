5687d681493e8a6accbed943e969e1d554612b76
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-24 05:36:07 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * swap_int - Swaps two integers a and b
- * @a : first int 
- * @b : second int 
- * Return: 0 is sucesss 
+ * @a : first int
+ * @b : second int
+ * Return: 0 is sucesss
  */
 
 void swap_int(int *a, int *b)
