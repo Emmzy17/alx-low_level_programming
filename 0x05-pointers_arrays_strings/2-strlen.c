02de832ecfa448712dfab465d99eb7543460b749
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-24 05:56:24 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - Gets the lenght of a string 
+ * @s - pointer to the stack section of the string 
+ * Return: 0 is alwats sucess 
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return count;
+}
+
