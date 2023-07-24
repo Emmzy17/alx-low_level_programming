83758c0416e374a31d9ad8bc5c248116a17db061
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-24 08:06:38 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 Prints every other character of a string
+ * @s: char to print
+ * Return: 0 is sucess
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0', i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+}
