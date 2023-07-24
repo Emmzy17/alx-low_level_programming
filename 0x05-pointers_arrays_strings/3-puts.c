5bd46fe3b0f6823053cfaf3516bca094ea7db8e3
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-24 06:35:22 -0700

    3-puts-correction

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _puts(char *str) - Prints a string
+ * _puts - Prints a string
  * @str : Address of the string
  * Return: 0 is always sucess
  */
 
 void _puts(char *str)
 {
-	while(*str != '\0')
+	while (*str != '\0')
 	{
 		_putchar(*str);
 		str++;
