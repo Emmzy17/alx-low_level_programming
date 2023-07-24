585d5f4b29771bb17c8860f99336335f0a4862ee
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-24 07:34:58 -0700

    reverse_stromg

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,22 @@
 #include "main.h"
 /**
  * rev_string - reverse a string
+ * _putchar - print each character
  * @s: String address on staacck
  */
 
 void rev_string(char *s)
 {
-	_puts(s);
-	print_rev(s);
+	int a = 0, b, c;
+	char d;
+
+	while (s[a] != '\0')
+		a++;
+	c = a - 1;
+	for (b = 0; c >=0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
 }
