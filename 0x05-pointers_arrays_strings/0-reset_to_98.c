05f73be5864e6c9311faeeb8d89e12f0d42da4d8
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-24 04:53:49 -0700

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and updates the value
+ * @n : pointer parameter
+ * Return: 0 is sucess
+ */
+
+void reset_to_98(int *n)
+{
+	*n=98;
+	return (0);
