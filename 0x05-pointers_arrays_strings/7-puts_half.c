d26c8db2c3209a6d8dfea761c8608dd4b19579dd
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-25 00:11:12 -0700

    puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,14 +5,24 @@
  * Return: 0 is sucess
  */
 
-void puts_half(char *s)
+void puts_half(char *str)
 {
-	int a = 0;
+	int index = 0, n;
 
-	while (*s != '\0')
-		a++;
-	int length = (a - 1) / 2;
-	for (length; s[length] != '\0';length++)
-		_putchar(s[length]);
-	_putchar('\n');
+	while (str[index] != '\0')
+		index++;
+	if (index  + 1 % 2 != '0')
+	{
+		n = (index - 1) / 2;
+	}
+	else
+	{
+		n = index / 2;
+	}		
+	n++;
+	for (index = n; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n')
 }
