aa7f4796177ac0fc7d4d6eba2626d6f93f32b3ef
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-07-25 02:48:30 -0700

    print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array
+ * @a: array
+ * @n: nth index of the array
+ * Retun: 0 is sucess
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		if (i == n)
+			prinf("%d", a[i]);
+		else
+			prinf("%d ", a[i]);
+}
