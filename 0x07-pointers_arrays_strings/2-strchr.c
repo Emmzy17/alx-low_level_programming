507657e164755b4edeb5b282f07a2b0b3ae2f281
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-01 18:16:22 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,23 +2,24 @@
 
 /**
  * _strchr - locates a character in string
+ *
  * @s: string
+ * 
  * @c: character to locate
+ * 
  * Return: character pointer
  */
 char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
-	if (*s != c)
-		s++;
-	else
-		return (s);
+		if (*s != c)
+			s++;
+		else
+			return (s);
 	}
-	if (c == '\0')
-	{
-	return (s);
-	}
-	
+		if (c == '\0')
+			return (s);
+
 	return (NULL);
 }
