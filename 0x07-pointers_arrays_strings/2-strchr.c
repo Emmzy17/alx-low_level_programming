04de24da16318c55dea3ebd5592fc66a0ed08530
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-01 18:03:18 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in string
+ * @s: string
+ * @c: character to locate
+ * Return: character pointer
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
