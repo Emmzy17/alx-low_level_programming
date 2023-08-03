cd585f42f47d6b7b830dba0c304d3022dd817662
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-03 13:53:01 -0700

    first_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string recursively
+ * @s: string pointer 
+ * Return: character of string
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+		_put_recursion(*s);
+}
