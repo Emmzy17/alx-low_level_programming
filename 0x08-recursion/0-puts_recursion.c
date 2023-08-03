0917d5d4af24397fe91d0f231576a1b148495ae4
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-03 14:26:15 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,16 @@
 
 /**
  * _puts_recursion - prints a string recursively
- * @s: string pointer 
+ * @s: string pointer
+ *
  * Return: character of string
  */
-
 void _puts_recursion(char *s)
 {
 	_putchar(*s);
 	s++;
 	if (*s != '\0')
 		_puts_recursion(s);
+	else
+		_putchar('\n');
 }
