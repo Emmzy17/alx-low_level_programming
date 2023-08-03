a94a96ac72b8f09790ad8612d03b21d94de08a58
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-03 15:11:39 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,10 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	if (*s != '\0')
-		_puts_recursion(s);
+	if (*s)
+		_putchar('\n');
+		_puts_recursion(s + 1);
+		
 	else
 		_putchar('\n');
 }
