795735203d45ae29a6e6bdfcb1f028afda596fde
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-04 08:27:13 -0700

    args_count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the nummber of arguement passed
+ * @argc: arguement count
+ * @argv: arguement parameters
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
