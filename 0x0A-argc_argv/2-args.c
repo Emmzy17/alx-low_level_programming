92888655a5904cf2af47e357f6fad516cbb93e7b
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-04 09:18:23 -0700

    print_args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguements it recieves
- * @argc - arguement count
- * @argv - arguements
+ * main - prints all arguments it receives.
+ * @argc: argument count
+ * @argv: arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{
 		printf("%s\n", argv[i]);
-	}
+
 	return (0);
 }
