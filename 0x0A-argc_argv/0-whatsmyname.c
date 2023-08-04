17eb97a6d57189ca81768d9c67e094c31831322c
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-04 04:43:18 -0700

    argv[0]

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - prints name of the prgram
  * @argc: Counts the command line arguements
  * @argv: stores the command line arguements
  * Return: 0 is success
  */
-
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
