bcd13ba888773076fad6a2b1f91fc4a0b58d2dac
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-04 10:15:54 -0700

    multiple_two_arguements

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two argument
+ * @argc: argument count
+ * @argv: arguements
+ *
+ * Return: 0 is sucess
+ */
+int main(int argc, char *argv[])
+{
+	int arg1 = strtol(argv[1], NULL, 10);
+	int arg2 = strtol(argv[2], NULL, 10);
+	
+	if (argc > 3 || argc < 3)
+	{	
+		printf("Error");
+		return (1);
+	}
+
+	(void)argc;
+	printf("%d\n", arg1 * arg2);
+	return(0);
+}
