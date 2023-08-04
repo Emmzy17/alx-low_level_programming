0fcbe04d1d42e5f8edfb12be9a7228dfef339aa1
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-04 05:08:25 -0700

    args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints name of the prgram
- * @argc: Counts the command line arguements
- * @argv: stores the command line arguements
+ * main - prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arguments
  *
  * Return: 0
  */
 int main(int argc, char **argv)
 {
-	void(argc);
+	(void)argc;
 
 	printf("%s\n", argv[0]);
 	return (0);
