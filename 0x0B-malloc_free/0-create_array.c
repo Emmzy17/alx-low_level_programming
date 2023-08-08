d73f4b4b766bba502fa979251a026244c81a42dc
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-08 14:38:02 -0700

    create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,14 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *array;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 	array = (char *)malloc(size * sizeof(char));
 	
 	if (array == NULL)
-		return NULL;
-
+		return (NULL);
 	for (i = 0; i < size; i++)
 		array[i] = c;
 
-	return array;
+	return (array);
 }
