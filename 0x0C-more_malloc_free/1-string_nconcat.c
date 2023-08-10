9bb62f3b75ad92356515e61346c43535c3e4c2ec
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-10 16:12:01 -0700

    string_concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		result[i] = s1[i];
 
 	for (j = 0; j < n; j++, i++)
-		result[i] = s2[j]
+		result[i] = s2[j];
 	result[i] = '\0';
 	
 	return (result);
