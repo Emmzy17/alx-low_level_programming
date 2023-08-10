08acaa47aaf1776aa6b2d69ddd7b28496e48f97d
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-10 10:31:12 -0700

    malloc_check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: Size of memory to be allocated
+ *
+ * Return: void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b * sizeof(unsigned int));
+
+	if (*mem == NULL)
+		return NULL;
+	return mem;
+}
