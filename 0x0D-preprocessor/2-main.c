145a011b6142d7d68b89f83aabaa2962f12f03c3
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-15 06:44:22 -0700

    2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * 2-main - this functions prints the file it was compiled from
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	while (__FILE__)
+		i++
+	for (j = 0; j < i; j++)
+		_putchar(j);
+	_putchar('\n');
+	return (0);
+}
