c2db5932de7e3ee9a77526a60ebe696d1504cbae
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-15 09:36:38 -0700

    print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_dog - prints struct dog
+ * @d: pointer to sturct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
