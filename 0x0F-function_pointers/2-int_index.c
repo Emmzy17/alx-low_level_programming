5b60232325ac0ba5977642327e81cb7cf6d521b3
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-17 04:59:45 -0700

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,16 +4,14 @@
  * @size: size of the array
  * cmp: pointer to function(cmp)
  */
-int int_inde(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || !cmp)
-		return;
-	if(size <= 0)
+	if (size <= 0 ||!array || !cmp)
 		return (-1);
 	for (i = 0; i < size; i++)
-		if (cmp[i] != 0)
+		if (cmp(array[i]) != 0)
 			return (i);
 	return (-1);
 }
