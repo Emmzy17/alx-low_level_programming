837777bef516deb218a7fc2244ee519bed4c3c64
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-25 14:29:20 -0700

    3-add_node_end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - add a new node to the end of the linked_list
+ * @head: pointer to pointer
+ * @str: string to add to new_node
+ *
+ * Return: pointer to new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
+
+	if(str == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = strlen(new->str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
+}
+
