2c6119414eaf9ead25983c1960cce78f9bacb121
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-31 15:16:56 -0700

    get_index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,11 +3,11 @@
  * get_bit - returns the value of a bit at a given index
  * @n: number toe be indexed
  * @index: index of the number n
- * Return: a number 
+ * Return: a number
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) *8)
+	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
