959dc3783048e00d4ee4f3853f5fcf5a23c4c727
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-31 15:05:07 -0700

    corrected_betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_binary - prints binary representation of a number 
+ * print_binary - prints binary representation of a number
  * @n: Number
  */
 void print_binary(unsigned long int n)
