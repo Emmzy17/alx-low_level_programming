b918f5ce644329a966b96e98266f8b7b4e9da029
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-31 14:11:13 -0700

    bit_operation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * binary_to_uint - Converts binary to an unsigned int
+ * @b: binary
+ * Return: Unsigned int or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	for (; *b != '\0'; ++b)
+	{
+		result <<= 1;
+
+		if (*b == '1')
+		{
+			result += 1;
+		}
+		else if (*b != 0)
+		{
+			return (0);
+		}
+	}
+
+	return (result);
+}
