c5a1221c5b4700986b339db78788221954487f83
Emmanuel Adoyi <emmanueladjerry@gmail.com>
2023-08-31 15:36:39 -0700

    set_bits

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,6 +3,7 @@
  * set_bit - sets the value of a bit to 1 at a given index
  * @n: the number
  * @index: index of number n
+ * Return: An int
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
